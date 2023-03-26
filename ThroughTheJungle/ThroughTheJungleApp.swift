6837a5e60a644e852a15bf5eebf6a9538b4965cd
Charel Felten <charel.felten@gmail.com>
2023-03-26 21:49:42 +0200

    Initial Commit

---
+++ ThroughTheJungle/ThroughTheJungleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThroughTheJungleApp.swift
+//  ThroughTheJungle
+//
+//  Created by Charel Felten on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThroughTheJungleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
